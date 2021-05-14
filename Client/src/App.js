03bd0870eb684deb3953756c136f9e005d82ca0d
Sai Ganesh Kodipelly <ganeshrao9949@gmail.com>
2021-05-15 01:56:01 +0530

    changed complete code

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,29 +1,44 @@
-import { React, useEffect, useState } from "react"
+import { React, useState } from "react"
+import Card from "./components/card"
+import Header from "./components/header"
+import Content from "./components/content"
 const axios = require("axios")
 
 const App = () => {
-  const [data, setData] = useState([]);
-  const [display, setDisplay] = useState(false)
+    const [data, setData] = useState([]);
+    const [display, setDisplay] = useState(false)
 
-  useEffect(() => {
     async function fetchData() {
-      try {
-        const response = await axios.get("/fetch");
-        setData(response.data);
-        setDisplay(true)
-      } catch (err) {
-        console.log(err.message);
-        setDisplay(false)
-      }
+        try {
+            const response = await axios.get("/fetch");
+            setData(response.data);
+            setDisplay(true)
+        } catch (err) {
+            console.log(err.message);
+            setDisplay(false)
+        }
     }
-    fetchData()
-  }, [data])
 
-  return (
-    <div>
-      {display ? <h1>Data fetched successfully</h1> : <h1>Reload the page once more</h1>}
-    </div>
-  )
+    if (data.length === 0) fetchData()
+
+    return (
+        <>
+            <Header />
+            <Content />
+            <div className="container">
+                {!display ?
+                    <h1>Reload the page once more</h1> :
+                    <div className="row">
+                        {
+                            data.map((info, index) => {
+                                return <Card key={index} info={info} />
+                            })
+                        }
+                    </div>
+                }
+            </div>
+        </>
+    )
 
 }
 
